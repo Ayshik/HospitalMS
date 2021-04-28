4181251791f49ba43439ed399ca9437b767ebbc4
MD WALI MOSNAD AYSHIK <61822224+Ayshik@users.noreply.github.com>
2021-04-29 00:38:35 +0600

    ajax on apointment list

--- Patient/View/ViewAppoinment.php
+++ Patient/View/ViewAppoinment.php
@@ -3,193 +3,44 @@ include('header/patheader.php');
 require_once ('../Controller/allapointment.php');
 $info=getallapointment();
 ?>
-<style>
-*{
-    box-sizing: border-box;
-    -webkit-box-sizing: border-box;
-    -moz-box-sizing: border-box;
-}
-body{
-    font-family: Helvetica;
-    -webkit-font-smoothing: antialiased;
-    background: rgba( 71, 147, 227, 1);
-}
-h2{
-    text-align: center;
-    font-size: 18px;
-    text-transform: uppercase;
-    letter-spacing: 1px;
-    color: white;
-    padding: 30px 0;
-}
-
-/* Table Styles */
-
-.table-wrapper{
-    margin: 10px 70px 70px;
-    
-}
-
-.fl-table {
-    border-radius: 5px;
-    font-size: 12px;
-    font-weight: normal;
-    border: none;
-    border-collapse: collapse;
-    width: 99%;
-    /* padding-top: 34px; */
-    /* align-items: center; */
-    max-width: 69%;
-    margin-left: 257px;
-    margin-top: -12px;
-    white-space: nowrap;
-    background-color: #1f80fd;
-}
-
-.fl-table td, .fl-table th {
-    text-align: center;
-    padding: 8px;
-}
-
-.fl-table td {
-    border-right: 1px solid #f8f8f8;
-    font-size: 12px;
-}
-
-.fl-table thead th {
-    color: #ffffff;
-    background: #4FC3A1;
-}
 
 
-.fl-table thead th:nth-child(odd) {
-    color: #ffffff;
-    background: #324960;
-}
 
-.fl-table tr:nth-child(even) {
-    background: #F8F8F8;
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ 
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</head>
+<body>
 
-/* Responsive */
-
-@media (max-width: 767px) {
-    .fl-table {
-        display: block;
-        width: 100%;
-    }
-    .table-wrapper:before{
-        content: "Scroll horizontally >";
-        display: block;
-        text-align: right;
-        font-size: 11px;
-        color: white;
-        padding: 0 0 10px;
-    }
-    .fl-table thead, .fl-table tbody, .fl-table thead th {
-        display: block;
-    }
-    .fl-table thead th:last-child{
-        border-bottom: none;
-    }
-    .fl-table thead {
-        float: left;
-    }
-    .fl-table tbody {
-        width: auto;
-        position: relative;
-        overflow-x: auto;
-    }
-    .fl-table td, .fl-table th {
-        padding: 20px .625em .625em .625em;
-        height: 60px;
-        vertical-align: middle;
-        box-sizing: border-box;
-        overflow-x: hidden;
-        overflow-y: auto;
-        width: 120px;
-        font-size: 13px;
-        text-overflow: ellipsis;
-    }
-    .fl-table thead th {
-        text-align: left;
-        border-bottom: 1px solid #f7f7f9;
-    }
-    .fl-table tbody tr {
-        display: table-cell;
-    }
-    .fl-table tbody tr:nth-child(odd) {
-        background: none;
-    }
-    .fl-table tr:nth-child(even) {
-        background: transparent;
-    }
-    .fl-table tr td:nth-child(odd) {
-        background: #F8F8F8;
-        border-right: 1px solid #E6E4E4;
-    }
-    .fl-table tr td:nth-child(even) {
-        border-right: 1px solid #E6E4E4;
-    }
-    .fl-table tbody td {
-        display: block;
-        text-align: center;
-    }
-}
-input[type=submit] {
-  background-color: #4CAF50;
-  color: white;
-  padding: 12px 20px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  float: right;
-  margin-right: 47%;
-}
-
-input[type=submit]:hover {
-  background-color: #45a049;
-}
-#myInput {
-  background-image: url('https://www.w3schools.com/css/searchicon.png');
-  background-position: 10px 10px;
-  background-repeat: no-repeat;
-  width: 50%;
-  font-size: 16px;
-  padding: 12px 20px 12px 40px;
-  border: 1px solid #ddd;
-margin-bottom: 17px;
-    margin-top: 22px;
-    margin-left: 422px;
-
-}
-</style>
-
-
-<section>
-<input type="text" id="myInput" onkeyup="myFunction()" placeholder="Type date of Appointment.." title="Type in a Date">
-
-<div class="table-wrapper">
-
-    <table id="myTable" class="fl-table">
-	
-        <thead>
-		
-                <th>Sl.</th>
+<div class="jumbotron text-center">
+  <h1>Appointment details</h1>
+</div>
+  
+<div class="container">
+  <div class="row">
+    <div class="col-sm-3">
+    </div>
+    <div class="col-sm-6">
+      <input type="text" class="form-control" id="search">
+      <table class="table table-hover">
+      <thead>
+        <tr>
+          <th>Sl.</th>
                   <th>UserName</th>
                 <th>Dr Name</th>
                
                 <th>Appointment time</th>
-                
-				
- 
-  
-
-              </thead>
-
-
-              <tbody>
-                <?php
+        </tr>
+      </thead>
+      <tbody id="output">
+         <?php
                   foreach($info as $infos)
                   {
                     echo "<tr>";
@@ -205,45 +56,29 @@ margin-bottom: 17px;
                     echo "</tr>";
                   }
                 ?>
+      </tbody>
+    </table>
+    </div>
+    <div class="col-sm-3">
+    </div>
+  </div>
+</div>
+<script type="text/javascript">
+  $(document).ready(function(){
+    $("#search").keypress(function(){
+      $.ajax({
+        type:'POST',
+        url:'../Controller/search.php',
+        data:{
+          name:$("#search").val(),
+        },
+        success:function(data){
+          $("#output").html(data);
+        }
+      });
+    });
+  });
+</script>
 
-              </tbody>
-
-
-
-
-
-
-
-
-            </table>
-          </div>
-        </div>
-      </div>
-	  </form>
-
-
-
-</section>
-  </body>
+</body>
 </html>
-<script>
- function myFunction() {
-  var input, filter, table, tr, td, i, txtValue;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("myTable");
-  tr = table.getElementsByTagName("tr");
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[3];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    }
-  }
-}
-
-        </script>
